cde4e6c85eafe9d1a4db0f6d71042c0fece0244e
Kacper Dygoń <dygonkacper@gmail.com>
2025-06-13 19:38:31 +0200

    added cmd args for language and export file to export.py

--- export.py
+++ export.py
@@ -1,22 +1,44 @@
 # pylint: skip-file
 import sqlite3
 import json
+import argparse
 
-database = 'words'
-table = 'words_polish'
-query = f'SELECT word FROM {table}'
-export_file = 'words.json'
+def main():
 
-connection = sqlite3.connect(database)
-cursor = connection.cursor()
+    parser = argparse.ArgumentParser(description="Skribbl.io scraper")
 
-cursor.execute(query)
-rows = cursor.fetchall()
+    parser.add_argument("--language", type=str, 
+                        required=True, help="Table that words are going to be exported from")
+    parser.add_argument("--export-file", type=str, 
+                        default="words.json", help="File that words are exported to")
 
-words = [row[0] for row in rows]
+    args = parser.parse_args()
 
-with open(export_file, 'w', encoding='utf-8') as file:
-    json.dump(words, file, ensure_ascii=False, indent = 2)
+    database = 'words'
+    table = f'words_{args.language.lower()}'
+    query = f'SELECT word FROM {table}'
+    export_file = args.export_file
 
-connection.close()
-print(f'{len(words)} were saved to {export_file}')
+    try:
+        connection = sqlite3.connect(database)
+        cursor = connection.cursor()
+        cursor.execute(query)
+    except sqlite3.OperationalError as e:
+        if "no such table" in str(e):
+            print("Table with words in selected language doesn't exist")
+            return
+        else:
+            raise
+
+    rows = cursor.fetchall()
+
+    words = [row[0] for row in rows]
+
+    with open(export_file, 'w', encoding='utf-8') as file:
+        json.dump(words, file, ensure_ascii=False, indent = 2)
+
+    connection.close()
+    print(f'{len(words)} words were saved to {export_file}')
+
+if __name__ == "__main__":
+    main()
