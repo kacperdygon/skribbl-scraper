7d003dc3f0179a1fe69d1d17e40f434366f5f23c
Kacper Dygoń <dygonkacper@gmail.com>
2025-06-13 10:44:51 +0200

    support for edge and safari

--- app/web_driver.py
+++ app/web_driver.py
@@ -1,11 +1,10 @@
 """File with web driver singleton"""
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.remote.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 class WebDriverManager:
     """Singleton with browser driver to support different browsers"""
@@ -24,21 +23,38 @@ class WebDriverManager:
 
     def _init_driver(self, browser, browser_profile_path):
 
-        if browser == "chrome":
-            options = webdriver.ChromeOptions()
-            if browser_profile_path:
-                options.add_argument(f"user-data-dir={browser_profile_path}")
-            service = ChromeService(ChromeDriverManager().install())
-            self.driver = webdriver.Chrome(service=service, options=options)
-        elif browser == "firefox":
-            options = FirefoxOptions()
-            if browser_profile_path:
-                options.add_argument("-profile")
-                options.add_argument(browser_profile_path)
-            service = FirefoxService(GeckoDriverManager().install())
-            self.driver = webdriver.Firefox(service=service, options=options)
-        else:
-            raise ValueError("Unsupported browser")
+        match browser:
+            case "chrome":
+                options = webdriver.ChromeOptions()
+                if browser_profile_path:
+                    user_data_dir = os.path.dirname(browser_profile_path)
+                    profile_dir = os.path.basename(browser_profile_path)
+                    options.add_argument(f"user-data-dir={user_data_dir}")
+                    options.add_argument(f"--profile-directory={profile_dir}")
+                service = webdriver.ChromeService(ChromeDriverManager().install())
+                self.driver = webdriver.Chrome(service=service, options=options)
+            case "firefox":
+                options = webdriver.FirefoxOptions()
+                if browser_profile_path:
+                    options.add_argument("-profile")
+                    options.add_argument(browser_profile_path)
+                service = webdriver.FirefoxService(GeckoDriverManager().install())
+                self.driver = webdriver.Firefox(service=service, options=options)
+            case "edge":
+                options = webdriver.EdgeOptions()
+                if browser_profile_path:
+                    user_data_dir = os.path.dirname(browser_profile_path)
+                    profile_dir = os.path.basename(browser_profile_path)
+                    options.add_argument(f"user-data-dir={user_data_dir}")
+                    options.add_argument(f"--profile-directory={profile_dir}")
+                service = webdriver.EdgeService(EdgeChromiumDriverManager().install())
+                self.driver = webdriver.Edge(service=service, options=options)
+            case "safari":
+                options = webdriver.SafariOptions()
+                service = webdriver.SafariService()
+                self.driver = webdriver.Safari(service=service, options=options)
+            case _:
+                raise ValueError("Unsupported browser")
 
     def get_driver(self) -> WebDriver:
         """Returns driver"""
